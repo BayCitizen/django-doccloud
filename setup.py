e23fe550129288bf97f09a4d90df111f42acc176
Aurelio D. Tinio <atinio@baycitizen.org>
2011-06-16 15:45:45 -0700

    add LICENSE, setup.py, move under package

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='django-doccloud',
+    version='0.1',
+    description='Provides a reusable document app which interfaces with DocumentCloud',
+    author='Bay Citizen',
+    author_email='info@baycitizen.org',
+    url='http://github.com/BayCitizen/django-doccloud/',
+    packages=[
+        'doccloud',
+    ],
+
+    install_requires=[
+        'distribute',
+    ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
