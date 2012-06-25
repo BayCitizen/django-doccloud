011562ddc141f2d4e449c9998aade1d374aa4a02
Shane Shifflett <shifflett.shane@gmail.com>
2012-06-25 11:51:33 -0700

    pep8 cleanup

--- doccloud/views.py
+++ doccloud/views.py
@@ -7,31 +7,42 @@ from doccloud.models import DocumentCloudProperties
 from doccloud.models import Document
 from django.contrib.auth.decorators import login_required
 
+
 #@login_required
 def create(request, template_name='upload.html'):
     context = {}
     context['form'] = DocCloudDocForm()
-    return render_to_response(template_name, context, context_instance=RequestContext(request))
+    return render_to_response(template_name,
+                              context,
+                              context_instance=RequestContext(request))
+
 
 def list(request, template_name='list.html'):
     context = {}
     context['objects'] = Document.objects.all()
-    return render_to_response(template_name, context, context_instance=RequestContext(request))
+    return render_to_response(template_name,
+                              context,
+                              context_instance=RequestContext(request))
+
 
 def detail(request, slug, template_name='detail.html'):
     '''
     Documents where access_level==Private will not appear in the detail page
-    unless the user is logged in.  Some JS logic will need to be added to the template
-    to check if the url: 
-    http://www.documentcloud.org/documents/{{document.dc_properties.dc_id}}.js exists
+    unless the user is logged in.
+    Some JS logic will need to be added to the template
+    to check if the url:
+    http://www.documentcloud.org/documents/{{document.dc_properties.dc_id}}.js
+    exists
     else fall back on the locally stored documents or use the secure url:
     https://www.documentcloud.org/documents/{{document.dc_properties.dc_id}}.js
     '''
     context = {}
     context['document'] = get_object_or_404(Document, slug=slug)
-    return render_to_response(template_name, context, context_instance=RequestContext(request))
+    return render_to_response(template_name,
+                              context,
+                              context_instance=RequestContext(request))
 
-#user field can be null so login not necessarily required
+
 #@login_required
 def upload(request, template_name='upload.html'):
     context = {}
@@ -41,15 +52,22 @@ def upload(request, template_name='upload.html'):
             dc_form.user = request.user
             if dc_form.is_valid():
                 model = dc_form.save(commit=False)
+                #user field can be null so login not necessarily required
                 #model.user = request.user
-                model.connect_dc_doc()#queue for background processing here
+                model.connect_dc_doc()  # queue for background processing here
                 model.save()
                 return redirect('docs_list')
             else:
                 context['form'] = dc_form
-                return render_to_response('upload.html', context, context_instance=RequestContext(request))
+                return render_to_response('upload.html',
+                                          context,
+                                          context_instance=RequestContext(request))
         else:
-            return render_to_response('upload.html', context, context_instance=RequestContext(request))
+            return render_to_response('upload.html',
+                                      context,
+                                      context_instance=RequestContext(request))
     except Exception as e:
-        print e#need logger
-    return render_to_response(template_name, context, context_instance=RequestContext(request))
+        print e  # need logger
+    return render_to_response(template_name,
+                              context,
+                              context_instance=RequestContext(request))
