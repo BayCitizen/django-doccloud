b37610bddc640c2ad940b77a7ffedcd05e77fd93
Shane Shifflett <shifflett.shane@gmail.com>
2012-06-25 11:48:11 -0700

    pep8 cleanup

--- doccloud/urls.py
+++ doccloud/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.defaults import url
 urlpatterns = patterns('',
     url(r'^$', 'doccloud.views.list', name='docs_list'),
     url(r'^list/$', 'doccloud.views.list', name='docs_list'),
-    url(r'^detail/(?P<slug>[\w-]+)/$', 'doccloud.views.detail', name='docs_detail'),
+    url(r'^detail/(?P<slug>[\w-]+)/$', 'doccloud.views.detail',
+        name='docs_detail'),
     url(r'^create/$', 'doccloud.views.create', name='docs_create'),
     url(r'^upload/$', 'doccloud.views.upload', name='docs_upload'),
 )
