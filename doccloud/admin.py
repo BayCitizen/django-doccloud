d966981ffdaf44774e8b81babed9d502d6733078
Shane Shifflett <shifflett.shane@gmail.com>
2012-06-25 11:36:23 -0700

    pep8 cleanup

--- doccloud/admin.py
+++ doccloud/admin.py
@@ -9,24 +9,24 @@ class DocumentAdmin(admin.ModelAdmin):
     exclude = ('user', 'dc_properties', )
 
     def save_model(self, request, obj, form, change):
-        if len(form.files) > 0 and obj.updated_at == None:
+        if len(form.files) > 0 and obj.updated_at is None:
             #file, obj are new
             obj.user = request.user
             obj.connect_dc_doc()
             obj.save()
-        elif len(form.files) > 0 and obj.updated_at != None:
+        elif len(form.files) > 0 and obj.updated_at is not None:
             #object has been updated, look for file changes
             n_file = form.files['file']
             n_file_sz = getsize(n_file)
             n_file_hdr = n_file.read(512) if n_file_sz >= 512\
-             else n_file.read()
+                else n_file.read()
             o_file = obj.file
             o_file_sz = getsize(o_file)
             o_file_hdr = o_file.read(512) if o_file_sz >= 512\
-             else o_file.read()
+                else o_file.read()
 
             if o_file_hdr != n_file_hdr:
-                #looks like the file could be different (not a whole file compar)
+                #our 512 byte comparison tells us the docs differ
                 obj.dc_properties.delete()
                 obj.connect_dc_doc()
                 obj.save()
